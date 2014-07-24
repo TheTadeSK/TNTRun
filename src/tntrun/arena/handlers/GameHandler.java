e5d278bb1d2564c40d42531b6f11aba7e31bd796
Shevchik <Shev4ik.den@gmail.com>
2014-07-24 23:51:08 +0400

    Check vector, it is faster

--- src/tntrun/arena/handlers/GameHandler.java
+++ src/tntrun/arena/handlers/GameHandler.java
@@ -200,7 +200,7 @@ public class GameHandler {
 		// check for lose
 		if (arena.getStructureManager().getLoseLevel().isLooseLocation(plloc)) {
 			// if we have the spectate spawn than we will move player to spectators, otherwise we will remove him from arena
-			if (arena.getStructureManager().getSpectatorSpawn() != null) {
+			if (arena.getStructureManager().getSpawnPointVector() != null) {
 				arena.getPlayerHandler().spectatePlayer(player, Messages.playerlosttoplayer, Messages.playerlosttoothers);
 			} else {
 				arena.getPlayerHandler().leavePlayer(player, Messages.playerlosttoplayer, Messages.playerlosttoothers);
